12c8e4766d3d2e76bfc5f8e03acfee61df20a411
Rajat <chauhan.rajat03@yahoo.com>
2021-10-05 00:10:08 +0530

    commiting to develop branch

--- test2ndge.ts
+++ test2ndge.ts
@@ -3,3 +3,13 @@ function chauhsaab(){
     console.log("jai ho");
     //ab yahan s 2nd vala likhega bro
 }
+function chauhsaab1(){
+
+    console.log("jai ho");
+    //ab yahan s 2nd vala likhega bro
+}
+function chauhsaab2(){
+
+    console.log("jai ho");
+    //ab yahan s 2nd vala likhega bro
+}
--- testfirstge.ts
+++ testfirstge.ts
@@ -1,4 +1,15 @@
-function chauhan(){
+function chauhsaab4(){
 
-    console.log("chauhan");
+    console.log("jai ho");
+    //ab yahan s 2nd vala likhega bro
+}
+function chauhsaab6(){
+
+    console.log("jai ho");
+    //ab yahan s 2nd vala likhega bro
+}
+function chauhsaab8(){
+
+    console.log("jai ho");
+    //ab yahan s 2nd vala likhega bro
 }
